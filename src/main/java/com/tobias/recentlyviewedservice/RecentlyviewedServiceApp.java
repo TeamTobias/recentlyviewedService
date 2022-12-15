677645833c9a4a07e7312ea1ea0f7537e673cdfa
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-15 17:08:09 +0900

    이슈 수정

--- src/main/java/com/tobias/recentlyviewedservice/RecentlyviewedServiceApp.java
+++ src/main/java/com/tobias/recentlyviewedservice/RecentlyviewedServiceApp.java
@@ -14,11 +14,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
 import tech.jhipster.config.DefaultProfileUtil;
 import tech.jhipster.config.JHipsterConstants;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigurationProperties({ ApplicationProperties.class })
 public class RecentlyviewedServiceApp {
 
